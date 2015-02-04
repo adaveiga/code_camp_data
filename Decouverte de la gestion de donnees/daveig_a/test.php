10593bae996e690e63f82dfef21d9821802a68f3
adam_a <adam_a@1586576b-5c7f-497b-855c-82c868351710>
2015-02-04 16:57:26 +0000

    utf8

--- Decouverte de la gestion de donnees/daveig_a/test.php
+++ Decouverte de la gestion de donnees/daveig_a/test.php
@@ -1,18 +1,27 @@
 <?php
-function lire_csv($nom_fichier, $separateur =";"){
+//header('Content-Type: application/json; charset=utf-8');
+
+function lire_csv($nom_fichier, $separateur =";") {
     $row = 0;
     $donnee = array();    
     $f = fopen($nom_fichier,"r");
     $taille = filesize($nom_fichier)+1;
     while ($donnee = fgetcsv($f, $taille, $separateur)) {
-        $result[$row] = $donnee;
-        $row++;
+    $result[$row] = $donnee;
+    $row++;
+    //$donnee[0] = utf8_encode("teét");
+    //echo $donnee[0] . " => " . utf8_decode($donnee[0]);
     }
     fclose ($f);
     return $result;
 }
 
-$result = lire_csv('Ciqual_2013.csv');
-var_dump($result);
+$result = lire_csv('liste.csv');
+
+print_r($result);
+
+$json = json_encode($result);
+
+file_put_contents("liste_json.json", $json);
 
 ?>
